d221cb6f750e1b392eeb984a541e9283450b9b8e
Ayw <2435406300@qq.com>
2025-02-14 10:46:05 +0800

    版本2

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "helloworld" << endl;
+    int count = 10;
+    for (int i = 0; i < count; i++)
+    {
+        cout << "helloworld" << endl;
+    }
+
     return 0;
 }
